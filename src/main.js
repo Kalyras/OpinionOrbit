8c14ad005d9c381ef3f451be5ac4a47bf268a8c0
Patricia <ps101@hdm-stuttgart.de>
2020-03-10 17:09:12 -0700

    add google analytics

--- src/main.js
+++ src/main.js
@@ -1,9 +1,14 @@
 import Vue from 'vue'
+import VueAnalytics from 'vue-analytics'
 import App from './App.vue'
 
 import './assets/css/style.css'
 
 Vue.config.productionTip = false
+
+Vue.use(VueAnalytics, {
+    id: 'UA-149909276-1'
+})
 
 new Vue({
     render: h => h(App),
